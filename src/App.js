1f71b2a1f2d31f523c61f1148af4bf3ecaab3c5b
Veeti <veeti@minibook>
2018-08-12 10:16:57 +0300

    it almost works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component, browserHistory } from 'react';
-import { BrowserRouter as Router, Link, NavLink, Route } from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import { Router, Link, NavLink, Route } from 'react-router-dom';
 import {Button} from './Components/contentHolders/contentHolders.js';
 
+import { createBrowserHistory } from 'history'
 
 import {Sidenav} from './Components/navbar/navbar.js';
 import {Social} from './Components/contentHolders/contentHolders.js';
@@ -17,6 +18,12 @@ import './scripts.js';
 
 //const history = createBrowserHistory()
 //history.listen(location => window.scrollTo(0, 0))
+
+const browserHistory = createBrowserHistory()
+browserHistory.listen((location, action) => {
+  window.scrollTo(0, 0);
+})
+
 
 const topics = [
 {
@@ -114,20 +121,26 @@ const topics = [
 
 
 
+
 function Resource({ match }) {
+  const topic = topics.find(({ id }) => id === match.params.topicId)
+  const subTopic = topic.resources.find(({ id }) => id === match.params.subId)
 
-
-  const topic = topics.find(({ id }) => id === match.params.topicId).resources.find(({ id }) => id === match.params.subId)
-
-
+  if (!topic) return null
 
   return(
-
-    <div id="scroll-to-top-article" className="article-holder-article touch">{topic.content}
-    <Social />
-    </div>
-
-
+    <Fragment>
+      { subTopic ?
+        <div id="scroll-to-top-article" className="article-holder-article touch">
+          { subTopic.content }
+          <Social />
+        </div>
+      :
+        <div className="article-holder-topic touch">
+          { topic.content }
+        </div>
+      }
+    </Fragment>
   )
 }
 
@@ -135,56 +148,24 @@ function Resource({ match }) {
 
 
 function Topic ({ match  }) {
-
   const topic = topics.find(({ id }) => id === match.params.topicId)
   return(
-
     <section className="submenu-and-article-holder">
 
-
-
       <div className="submenu-holder">
-      <ul>
-      <NavLink to={`${match.url }`}>
-      <div className="subtitle-box subtitle-box-subtitle">
-        <div className="subtopic-wrapper">
-          <p>{match.params.topicId}</p>
-        </div>
-      </div>
-      </NavLink>
-
-        {topic.resources.map((sub) => (
-        <li key={sub.id}>
-          <NavLink to={`${match.url }/${sub.id}`} activeClassName="selected-submenu">
-          <div className="subtitle-box fade">
-          <div className="number-wrapper">
-            {sub.subnumber}
-          </div>
-            <div className="text-wrapper">
-              {sub.name}
+        <ul>
+          <NavLink to={`${match.url }`}>
+            <div className="subtitle-box subtitle-box-subtitle">
+              <div className="subtopic-wrapper">
+                <p>{match.params.topicId}</p>
+              </div>
             </div>
-          </div>
           </NavLink>
-        </li>
-        ))}
-        </ul>
 
-      </div>
-
-      <div className="article-holder">
-        <Route path={`${match.path }/:subId`} component={Resource} />
-
-
-          <div className="article-holder-topic touch">
-          {topic.content}
-          <div className="submenu-holder2">
-
-          <ul>
-
-            {topic.resources.map((sub) => (
+          {topic.resources.map((sub) => (
             <li key={sub.id}>
-              <Link to={`${match.url }/${sub.id}`}>
-              <div className="subtitle-box">
+              <NavLink to={`${match.url }/${sub.id}`} activeClassName="selected-submenu">
+              <div className="subtitle-box fade">
               <div className="number-wrapper">
                 {sub.subnumber}
               </div>
@@ -192,28 +173,17 @@ function Topic ({ match  }) {
                   {sub.name}
                 </div>
               </div>
-              </Link>
+              </NavLink>
             </li>
+          ))}
+        </ul>
 
-            ))}
-            <li>
+      </div>
 
-                <div className="subtitle-box-for-button">
-
-                     <Button next={ topic.next } buttonText= { topic.buttonText } />
-
-                </div>
-
-            </li>
-            </ul>
-          </div>
-
-
-
-
-
-        </div>
-        </div>
+      <div className="article-holder">
+        { false && <Resource subId={match.params.subId} topicId={match.params.topicId} /> }
+        <Route path={`${match.path }/:subId?`} component={Resource} />
+      </div>
 
       </section>
 
@@ -228,51 +198,35 @@ function Topic ({ match  }) {
 function Topics ({ match  }) {
   function openNav(){
        document.getElementById("mySidenav").className = "sidenav-visible";
-
   }
-
 
   return (
     <section className="menu-and-topic-holder">
       <Sidenav />
 
       <div className="menu-holder">
+        <div className="number-box number-box-navmenu" onClick={openNav}>
+          <i className="fas fa-bars"></i>
+        </div>
 
-      <div className="number-box number-box-navmenu" onClick={openNav}>
-        <i className="fas fa-bars"></i>
+        { topics.map(( { name, id, number}) => (
+          <NavLink key={id} to={`/topics/${id}`} activeClassName="selected-menu">
+            <div className="number-box number-box-content-itself">
+              <div className="number-box-number ">
+                <h1>{ number }</h1>
+              </div>
+              <div className="number-box-title ">
+                <h1>{ name }</h1>
+              </div>
+            </div>
+          </NavLink>
+        ))}
 
       </div>
-
-        {topics.map(({ name, id, number}) => (
-
-          <NavLink key={id} to={`${match.url }/${id}`} activeClassName="selected-menu">
-            <div className="number-box number-box-content-itself">
-
-            <div className="number-box-number ">
-                <h1>
-                 {number}
-                </h1>
-            </div>
-
-                        <div className="number-box-title ">
-                            <h1>
-                             {name}
-                            </h1>
-                        </div>
-
-
-
-            </div>
-
-          </NavLink>
-
-          ))}
-
-        </div>
-        <div className="topic-holder">
-          <Route path={`${match.path}/:topicId`} component={Topic} />
-        </div>
-      </section>
+      <div className="topic-holder">
+        <Route path={`${match.path}/:topicId`} component={Topic} />
+      </div>
+    </section>
   )
 }
 
